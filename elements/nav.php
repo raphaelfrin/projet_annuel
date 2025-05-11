7a0bbad85111042267db3c7548b8adfbaa74614a
lilian-17 <lilian.martpro@gmail.com>
2025-05-11 16:16:43 +0200

    ajoute habitude
    j'arrive pas a inserer dans la bdd

---
+++ elements/nav.php
@@ -0,0 +1,11 @@
+<?php
+echo 
+'
+        <nav>
+            <a href="index.php">dashbord</a>
+            <a href="tasks.php">tâche</a>
+            <a href="habitude.php">habitude</a>
+            <a href="entreprise.php">entreprise</a>
+        </nav>
+'
+?>
--- html/signup.php
+++ html/signup.php
@@ -12,12 +12,9 @@
         <a id="conne" href="login.php">Se connecter</a>
     </div>
 
-    <nav>
-        <a href="index.php">dashbord</a>
-        <a href="tasks.php">tache</a>
-        <a href="habitude.php">habitude</a>
-        <a href="entreprise.php">entreprise</a>
-    </nav>
+    <?php
+include '../elements/nav.php';
+    ?>
 </header>
 
 <body>
